f75078c6a28ae43d2597d222ee0ac8bd6fe5a47e
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-12-02 17:36:39 -0500

    feat(cors)

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,14 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin:
-      configService.get('FRONTEND_URL') ||
-      configService.get('FRONTEND_URL_EXTEND') ||
+    origin: [
+      configService.get('FRONTEND_URL'),
+      configService.get('FRONTEND_URL_EXTEND'),
       'http://localhost:3000',
+    ],
     credentials: true,
   });
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
-
-//Date limit
