bb8eae6157ecb88d7bdafd5fc2b91e5a295dafe1
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-12-02 14:04:19 -0500

    add(configModule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnswerModule } from './answer/answer.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AnswerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AnswerModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: configService.get('FRONTEND_URL') || 'http://localhost:3000',
+    credentials: true,
+  });
+  const port = configService.get('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
 
