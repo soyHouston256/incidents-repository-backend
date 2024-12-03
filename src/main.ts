71158e99339b0d1b15ec21a8bb7d59861a81caf5
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-12-03 02:58:57 -0500

    fix(cors)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin: true,
+    origin: [
+      configService.get('FRONTEND_URL'),
+      configService.get('FRONTEND_URL_EXTEND'),
+    ],
     allowedHeaders: ['Content-Type', 'Accept'],
     methods: ['POST', 'OPTIONS'],
     credentials: true,
